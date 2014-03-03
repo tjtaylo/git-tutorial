2ac0280aa6872809b42de3e3bc113781e67a93a8
root <root@wdoy.net>
2014-03-03 13:06:08 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Git Tutorial for deployments</title>
+</head>
+<body>
+	<h1>Git Tutorial for Deployments</h1>
+	<p>I hope this works.</p>
+</body>
+</html>
