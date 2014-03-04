0a3449163f3799a7e814007f9b80a0c72aabfcb9
Travis Taylor <travis@webdesignofyork.com>
2014-03-04 09:08:57 -0500

    new post

--- index.php
+++ index.php
@@ -10,5 +10,14 @@
 	<p>Did it work?</p>
 	<p>Yes! It works!</p>
 	<p>Testing credential.helper cache <strong>[this is the test]</strong> <em>(test failed)</em></p>
+
+	<div id="blog">
+		<h2>Blog</h2>
+		<div class="article">
+			<h3>Working on more git</h3>
+			<em>March 4th, 2014</em>
+			<p>Planning to add more posts each day using sublime, iterm, and git to deploy. Practice makes perfect.</p>
+		</div>
+	</div>
 </body>
 </html>
